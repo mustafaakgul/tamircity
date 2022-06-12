3cfcfe5ec242738427732ee3bbb9dc8c400aabeb
TheOryZ <kadirrturan@gmail.com>
2022-06-12 21:45:06 +0300

    Add jwt-service.go
    Add jwt-service.go

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/mustafakocatepe/Tamircity
 go 1.18
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/joho/godotenv v1.4.0
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
 	gorm.io/driver/postgres v1.3.7
---
+++ pkg/service/jwt-service.go
@@ -0,0 +1,67 @@
+package service
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+//JWTService interface
+type JWTService interface {
+	GenerateToken(userID string) string
+	ValidateToken(token string) (*jwt.Token, error)
+}
+
+type jwtCustomClaims struct {
+	UserID string `json:"user_id"`
+	jwt.StandardClaims
+}
+
+type jwtService struct {
+	secretKey string
+	issuer    string
+}
+
+func NewJWTService() JWTService {
+	return &jwtService{
+		secretKey: getSecretKey(),
+		issuer:    "http://localhost:8080",
+	}
+}
+
+func getSecretKey() string {
+	secretKey := os.Getenv("JWT_SECRET")
+	if secretKey == "" {
+		secretKey = "JohnDoeSecrets"
+	}
+	return secretKey
+}
+
+func (j *jwtService) GenerateToken(userID string) string {
+	claims := &jwtCustomClaims{
+		userID,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().AddDate(0, 0, 1).Unix(),
+			Issuer:    j.issuer,
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	t, err := token.SignedString([]byte(j.secretKey))
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func (j *jwtService) ValidateToken(token string) (*jwt.Token, error) {
+	return jwt.Parse(token, func(t_ *jwt.Token) (interface{}, error) {
+		if _, ok := t_.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method %v", t_.Header["alg"])
+		}
+		return []byte(j.secretKey), nil
+	})
+}
