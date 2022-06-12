3d296d6d5f1aed4cd9312ae3fd1b4a958ffc979a
TheOryZ <kadirrturan@gmail.com>
2022-06-12 18:07:37 +0300

    Added Repository classes
    Added Repository classes

--- pkg/models/db/deviceType.go
+++ pkg/models/db/deviceType.go
@@ -2,11 +2,13 @@ package db
 
 import "gorm.io/gorm"
 
-type Model struct {
+type DeviceType struct {
 	gorm.Model
 	Name              string `json:"name" gorm:"type:varchar(255);not null"`
 	Short_Description string `json:"short_description" gorm:"type:varchar(255);"`
-	Brand_Id          int    `json:"brand_id"`
-	Brand             Brand  `json:"brand" gorm:"foreignkey:BrandId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	Is_Active         bool   `json:"is_active" gorm:"type:boolean;default:true;not null"`
+}
+
+func (DeviceType) TableName() string {
+	return "deviceTypes"
 }
--- pkg/models/db/fixType.go
+++ pkg/models/db/fixType.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type FixType struct {
 	gorm.Model
 	Description string `json:"description" gorm:"type:varchar(255);not null"`
-	IsActive    bool   `json:"is_active" gorm:"type:boolean;not null"`
+	Is_Active   bool   `json:"is_active" gorm:"type:boolean;default:true;not null"`
 }
 
 func (FixType) TableName() string {
