29fee70122f3395911ba598013f6b900d0bc3751
TheOryZ <kadirrturan@gmail.com>
2022-06-18 14:05:56 +0300

    Update main.go

--- cmd/tamircity/main.go
+++ cmd/tamircity/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/mustafakocatepe/Tamircity/handler/api"
 	"github.com/mustafakocatepe/Tamircity/pkg/service"
 	"github.com/mustafakocatepe/Tamircity/pkg/store/repositories"
-	"log"
 
 	"github.com/joho/godotenv"
 	dbModels "github.com/mustafakocatepe/Tamircity/pkg/models/db"
@@ -30,14 +31,26 @@ func main() {
 	//Store
 	serviceTypeStore := repositories.NewServiceTypeStore(db)
 	extraServiceStore := repositories.NewExtraServiceStore(db)
+	brandStore := repositories.NewBrandRepository(db)
+	modelStore := repositories.NewModelRepository(db)
+	fixTypeStore := repositories.NewFixTypeRepository(db)
+	deviceTypeStore := repositories.NewDeviceTypeRepository(db)
 
 	//Service
 	serviceTypeService := service.NewServiceTypeService(serviceTypeStore)
 	extraServiceService := service.ExtraServiceService(extraServiceStore)
+	brandService := service.NewBrandService(brandStore)
+	modelService := service.NewModelService(modelStore)
+	fixTypeService := service.NewFixTypeService(fixTypeStore)
+	deviceTypeService := service.NewDeviceTypeService(deviceTypeStore)
 
 	//Handler
 	serviceTypeHandler := api.NewServiceTypeHandler(serviceTypeService)
 	extraServiceHandler := api.NewExtraServiceHandler(extraServiceService)
+	brandHandler := api.NewBrandHandler(brandService)
+	modelHandler := api.NewModelHandler(modelService)
+	fixTypeHandler := api.NewFixTypeHandler(fixTypeService)
+	deviceTypeHandler := api.NewDeviceTypeHandler(deviceTypeService)
 
 	//gin server
 	router := gin.Default()
@@ -47,6 +60,10 @@ func main() {
 	{
 		route.GET("/service_type", serviceTypeHandler.GetAll)
 		route.GET("/extra_service", extraServiceHandler.GetAll)
+		route.GET("/brand", brandHandler.GetAll)
+		route.GET("/model", modelHandler.GetAll)
+		route.GET("/fix_type", fixTypeHandler.GetAll)
+		route.GET("/device_type", deviceTypeHandler.GetAll)
 	}
 
 	router.Run(":8080")
