da5f53e5e0f48b598f206be9b50358ddd94f051e
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2022-07-14 02:06:58 +0300

    add reservation

--- pkg/models/db/reservation.go
+++ pkg/models/db/reservation.go
@@ -1,7 +1,21 @@
 package db
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Reservation struct {
 	gorm.Model
+	DeviceTypeId    string `gorm:"not null"`
+	BrandId         string `gorm:"not null"`
+	ModelId         string `gorm:"not null"`
+	FixTypeId       string `gorm:"not null"`
+	ServiceTypeId   string `gorm:"not null"`
+	ExtraServiceId  string `gorm:"not null"`
+	ReservationDate time.Time
+	FullName        string `gorm:"not null"`
+	Email           string `gorm:"not null"`
+	PhoneNumber     string `gorm:"not null"`
+	Description     string
 }
