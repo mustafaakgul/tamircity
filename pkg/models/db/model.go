e33f62282fc0c72dc7209704c8e777d067092d38
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2022-08-29 17:56:10 +0300

    update bug fix for Model

--- pkg/models/db/model.go
+++ pkg/models/db/model.go
@@ -4,12 +4,12 @@ import "gorm.io/gorm"
 
 type Model struct {
 	gorm.Model
-	Name             string `gorm:"type:varchar(255);not null"`
-	ShortDescription string `gorm:"type:varchar(255);"`
-	BrandId          int
-	Brand            Brand `gorm:"foreignkey:BrandId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	DeviceTypeId     int
-	DeviceType       DeviceType          `gorm:"foreignkey:DeviceTypeId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Name             string              `gorm:"type:varchar(255);not null"`
+	ShortDescription string              `gorm:"type:varchar(255);"`
+	BrandId          int                 `gorm:"default:null;"`
+	Brand            Brand               `gorm:"foreignkey:BrandId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL,default:null;"`
+	DeviceTypeId     int                 `gorm:"default:null;"`
+	DeviceType       DeviceType          `gorm:"foreignkey:DeviceTypeId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL,default:null;"`
 	IsActive         bool                `gorm:"type:boolean;default:true;not null"`
 	TechnicalService []*TechnicalService `gorm:"many2many:technical_services_models;"`
 	Reservations     []*Reservation
