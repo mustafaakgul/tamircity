d64c1072a49f7057a9fcd641b9ca9c94a3a08280
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2022-08-29 19:34:25 +0300

    update contact

--- pkg/models/db/contact.go
+++ pkg/models/db/contact.go
@@ -6,6 +6,6 @@ type Contact struct {
 	gorm.Model
 	FullName    string `gorm:"type:varchar(255);not null"`
 	Email       string `gorm:"type:varchar(50);not null"`
-	PhoneNumber string `gorm:"type:varchar(15);not null"`
+	PhoneNumber string `gorm:"type:varchar(50);not null"`
 	Description string `gorm:"type:varchar(255);not null"`
 }
