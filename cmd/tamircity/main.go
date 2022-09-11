ebcdf288de9b1684ad1733ff956ca315fc20ef2e
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2022-09-11 22:45:04 +0300

    update main problem

--- cmd/tamircity/main.go
+++ cmd/tamircity/main.go
@@ -1,105 +1,105 @@
-package main
-
-import (
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"github.com/mustafakocatepe/Tamircity/api/handler"
-	"github.com/mustafakocatepe/Tamircity/api/routes"
-	"github.com/mustafakocatepe/Tamircity/pkg/service"
-	"github.com/mustafakocatepe/Tamircity/pkg/store/repositories"
-	postgres "github.com/mustafakocatepe/Tamircity/pkg/store/shared/db"
-	"log"
-)
-
-func main() {
-	// Set enviroment variables
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	db, err := postgres.NewPsqlDB()
-	if err != nil {
-		log.Fatal("Postgres cannot init", err)
-	}
-	log.Println("Postgres connected")
-
-	// Connection DB and migrations
-	postgres.Migrate(db)
-
-	// TODO: Migrating
-	//postgres.Seeder(db)  //TODO
-	/*db.AutoMigrate(
-		&dbModels.Brand{},
-		&dbModels.Contact{},
-		&dbModels.Customer{},
-		&dbModels.DeviceType{},
-		&dbModels.ExtraService{},
-		&dbModels.FixType{},
-		&dbModels.Model{},
-		&dbModels.Newsletter{},
-		&dbModels.Reservation{},
-		&dbModels.ServiceType{},
-		&dbModels.TechnicalService{},
-	)*/
-	log.Println("Migrations done")
-
-	// Store
-	technicalServiceStore := repositories.NewTechnicalServiceStore(db)
-	serviceTypeStore := repositories.NewServiceTypeStore(db)
-	extraServiceStore := repositories.NewExtraServiceStore(db)
-	brandStore := repositories.NewBrandStore(db)
-	modelStore := repositories.NewModelStore(db)
-	reservationStore := repositories.NewReservationStore(db)
-	fixTypeStore := repositories.NewFixTypeStore(db)
-	deviceTypeStore := repositories.NewDeviceTypeStore(db)
-
-	// TODO: Adding Seed data
-	//technicalServiceStore.Seed()
-
-	// Clients
-	// This one need to be integrated systems
-
-	// Service
-	technicalServiceService := service.NewTechnicalServiceService(technicalServiceStore)
-	serviceTypeService := service.NewServiceTypeService(serviceTypeStore)
-	extraServiceService := service.ExtraServiceService(extraServiceStore)
-	brandService := service.NewBrandService(brandStore)
-	modelService := service.NewModelService(modelStore)
-	fixTypeService := service.NewFixTypeService(fixTypeStore)
-	deviceTypeService := service.NewDeviceTypeService(deviceTypeStore)
-	reservationService := service.NewReservationService(reservationStore)
-
-	// Handler
-	technicalServiceHandler := handler.NewTechnicalServiceHandler(technicalServiceService)
-	serviceTypeHandler := handler.NewServiceTypeHandler(serviceTypeService)
-	extraServiceHandler := handler.NewExtraServiceHandler(extraServiceService)
-	brandHandler := handler.NewBrandHandler(brandService)
-	modelHandler := handler.NewModelHandler(modelService)
-	fixTypeHandler := handler.NewFixTypeHandler(fixTypeService)
-	deviceTypeHandler := handler.NewDeviceTypeHandler(deviceTypeService)
-	reservationHandler := handler.NewReservationHandler(reservationService)
-
-	// gin server
-	router := gin.Default()
-	router.Use(gin.Logger())
-
-	// CORS Policy
-	corsConfig := cors.DefaultConfig()
-	corsConfig.AllowOrigins = []string{"*"}
-	corsConfig.AllowCredentials = true
-	corsConfig.AddAllowMethods("OPTIONS", "GET", "PUT", "PATCH", "POST")
-	router.Use(cors.New(corsConfig))
-
-	// Routes
-	routes.BrandRouter(router, brandHandler)
-	routes.DeviceTypeRouter(router, deviceTypeHandler)
-	routes.ExtraServiceRouter(router, extraServiceHandler)
-	routes.FixTypeRouter(router, fixTypeHandler)
-	routes.ModelRouter(router, modelHandler)
-	routes.ServiceTypeRouter(router, serviceTypeHandler)
-	routes.TechnicalServiceRouter(router, technicalServiceHandler)
-	routes.ReservationRouter(router, reservationHandler)
-
-	router.Run(":8080")
-}
+package tamircity //main old
+//
+//import (
+//	"github.com/gin-contrib/cors"
+//	"github.com/gin-gonic/gin"
+//	"github.com/joho/godotenv"
+//	"github.com/mustafakocatepe/Tamircity/api/handler"
+//	"github.com/mustafakocatepe/Tamircity/api/routes"
+//	"github.com/mustafakocatepe/Tamircity/pkg/service"
+//	"github.com/mustafakocatepe/Tamircity/pkg/store/repositories"
+//	postgres "github.com/mustafakocatepe/Tamircity/pkg/store/shared/db"
+//	"log"
+//)
+//
+//func main() {
+//	// Set enviroment variables
+//	err := godotenv.Load(".env")
+//	if err != nil {
+//		log.Fatal("Error loading .env file")
+//	}
+//	db, err := postgres.NewPsqlDB()
+//	if err != nil {
+//		log.Fatal("Postgres cannot init", err)
+//	}
+//	log.Println("Postgres connected")
+//
+//	// Connection DB and migrations
+//	postgres.Migrate(db)
+//
+//	// TODO: Migrating
+//	//postgres.Seeder(db)  //TODO
+//	/*db.AutoMigrate(
+//		&dbModels.Brand{},
+//		&dbModels.Contact{},
+//		&dbModels.Customer{},
+//		&dbModels.DeviceType{},
+//		&dbModels.ExtraService{},
+//		&dbModels.FixType{},
+//		&dbModels.Model{},
+//		&dbModels.Newsletter{},
+//		&dbModels.Reservation{},
+//		&dbModels.ServiceType{},
+//		&dbModels.TechnicalService{},
+//	)*/
+//	log.Println("Migrations done")
+//
+//	// Store
+//	technicalServiceStore := repositories.NewTechnicalServiceStore(db)
+//	serviceTypeStore := repositories.NewServiceTypeStore(db)
+//	extraServiceStore := repositories.NewExtraServiceStore(db)
+//	brandStore := repositories.NewBrandStore(db)
+//	modelStore := repositories.NewModelStore(db)
+//	reservationStore := repositories.NewReservationStore(db)
+//	fixTypeStore := repositories.NewFixTypeStore(db)
+//	deviceTypeStore := repositories.NewDeviceTypeStore(db)
+//
+//	// TODO: Adding Seed data
+//	//technicalServiceStore.Seed()
+//
+//	// Clients
+//	// This one need to be integrated systems
+//
+//	// Service
+//	technicalServiceService := service.NewTechnicalServiceService(technicalServiceStore)
+//	serviceTypeService := service.NewServiceTypeService(serviceTypeStore)
+//	extraServiceService := service.ExtraServiceService(extraServiceStore)
+//	brandService := service.NewBrandService(brandStore)
+//	modelService := service.NewModelService(modelStore)
+//	fixTypeService := service.NewFixTypeService(fixTypeStore)
+//	deviceTypeService := service.NewDeviceTypeService(deviceTypeStore)
+//	reservationService := service.NewReservationService(reservationStore)
+//
+//	// Handler
+//	technicalServiceHandler := handler.NewTechnicalServiceHandler(technicalServiceService)
+//	serviceTypeHandler := handler.NewServiceTypeHandler(serviceTypeService)
+//	extraServiceHandler := handler.NewExtraServiceHandler(extraServiceService)
+//	brandHandler := handler.NewBrandHandler(brandService)
+//	modelHandler := handler.NewModelHandler(modelService)
+//	fixTypeHandler := handler.NewFixTypeHandler(fixTypeService)
+//	deviceTypeHandler := handler.NewDeviceTypeHandler(deviceTypeService)
+//	reservationHandler := handler.NewReservationHandler(reservationService)
+//
+//	// gin server
+//	router := gin.Default()
+//	router.Use(gin.Logger())
+//
+//	// CORS Policy
+//	corsConfig := cors.DefaultConfig()
+//	corsConfig.AllowOrigins = []string{"*"}
+//	corsConfig.AllowCredentials = true
+//	corsConfig.AddAllowMethods("OPTIONS", "GET", "PUT", "PATCH", "POST")
+//	router.Use(cors.New(corsConfig))
+//
+//	// Routes
+//	routes.BrandRouter(router, brandHandler)
+//	routes.DeviceTypeRouter(router, deviceTypeHandler)
+//	routes.ExtraServiceRouter(router, extraServiceHandler)
+//	routes.FixTypeRouter(router, fixTypeHandler)
+//	routes.ModelRouter(router, modelHandler)
+//	routes.ServiceTypeRouter(router, serviceTypeHandler)
+//	routes.TechnicalServiceRouter(router, technicalServiceHandler)
+//	routes.ReservationRouter(router, reservationHandler)
+//
+//	router.Run(":8080")
+//}
