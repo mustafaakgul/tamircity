40df3c293cb8817b060d377f8693a7d02fce4872
Mustafa Kocatepe <msaidkocatepe@gmail.com>
2022-09-18 18:11:59 +0300

    add service type response model"

--- pkg/models/web/service_type.go
+++ pkg/models/web/service_type.go
@@ -1,1 +1,8 @@
 package web
+
+type ServiceTypeResponse struct {
+	Id          uint   `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       int64  `json:"price"`
+}
