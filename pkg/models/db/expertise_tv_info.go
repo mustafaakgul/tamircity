c031dbe37b07d80971456c54f3850c5eb65f5b74
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2022-10-10 14:43:35 +0300

    add expertise tv entity

--- pkg/models/db/expertise_tv_info.go
+++ pkg/models/db/expertise_tv_info.go
@@ -1,1 +1,54 @@
 package db
+
+import "gorm.io/gorm"
+
+type ExpertiseTvInfo struct {
+	gorm.Model
+	ReservationId             uint
+	Reservation               *Reservation `gorm:"foreignkey:ReservationId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Invoice                   bool         //Fatura var mi
+	Box                       bool         //Kutu
+	GuaranteeTerm             int          //1-24
+	GuaranteeTermType         string
+	Color                     string
+	SmartTv                   bool
+	PanelTechnology           string
+	CurvedScreen              bool
+	OSType                    string
+	InternalSatelliteReceiver bool
+	ScreenSize                float32
+	Ambilight                 bool
+	HDR                       bool
+	FullArray                 bool
+	ScreenResolution          string
+	RefreshRate               int
+	ReleaseYear               int
+	ScreenShare               bool
+	WiFi                      bool
+	RFInput                   bool
+	LAN                       bool
+	HeadphoneJack             bool
+	Bluetooth                 bool
+	HDMI                      bool
+	USB                       bool
+
+	IsScreenHasObscurationProblem      bool
+	IsScreenHasDeadPixelProblem        bool
+	IsScreenHasBrokenProblem           bool
+	IsScreenHasDownfallProblem         bool
+	IsScreenHasBandProblem             bool
+	IsScreenHasFreezingProblem         bool
+	IsDeviceHasHighHeatProblem         bool
+	IsDeviceHasSoundProblem            bool
+	IsDeviceHasInternalReceiverProblem bool
+	IsDeviceHasSpeakerProblem          bool
+	IsScreenShareHasProblem            bool
+	IsWiFiHasProblem                   bool
+	IsRFInputHasProblem                bool
+	IsEthernetHasProblem               bool
+	IsHeadphoneJackHasProblem          bool
+	IsBluetoothHasProblem              bool
+	IsHDMIHasProblem                   bool
+	IsUSBHasProblem                    bool
+	IsRemoteControlHasProblem          bool
+}
