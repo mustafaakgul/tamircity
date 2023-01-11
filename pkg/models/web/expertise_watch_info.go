2355106f4804fb63bc8dab519cb7d2be8b683c92
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-01-11 15:55:22 +0300

    update expertise watch web

--- pkg/models/web/expertise_watch_info.go
+++ pkg/models/web/expertise_watch_info.go
@@ -1,37 +1,37 @@
 package web
 
 type ExpertiseWatchInfoRequest struct {
-	ReservationId    uint
-	Invoice          bool
-	Box              bool
-	GuaranteeTerm    int
-	PhoneColor       string
-	Microphone       bool
-	ScreenSize       float32
-	Memory           int
-	ScreenType       string
-	OSType           string
-	OSTypeCompatiple string
-	ScreenResolution string
-	CPUFrequency     int
-	BatteryCapacity  int
-	Wifi             bool
-	Speaker          bool
-	SIMSupport       bool
-	DustProof        bool
-	NFC              bool
+	ReservationId    uint    `json:"reservation_id"`
+	Invoice          bool    `json:"invoice"`
+	Box              bool    `json:"box"`
+	GuaranteeTerm    int     `json:"guarantee_term"`
+	PhoneColor       string  `json:"phone_color"`
+	Microphone       bool    `json:"microphone"`
+	ScreenSize       float32 `json:"screen_size"`
+	Memory           int     `json:"memory"`
+	ScreenType       string  `json:"screen_type"`
+	OSType           string  `json:"os_type"`
+	OSTypeCompatiple string  `json:"os_type_compatiple"`
+	ScreenResolution string  `json:"screen_resolution"`
+	CPUFrequency     int     `json:"cpu_frequency"`
+	BatteryCapacity  int     `json:"battery_capacity"`
+	Wifi             bool    `json:"wifi"`
+	Speaker          bool    `json:"speaker"`
+	SIMSupport       bool    `json:"sim_support"`
+	DustProof        bool    `json:"dust_proof"`
+	NFC              bool    `json:"nfc"`
 
-	IsScreenHasBrokenProblem      bool
-	IsScreenHasObscurationProblem bool
-	IsTouchScreenHasProblem       bool
-	IsScreenHasDeadPixelPixel     bool
-	IsDeviceHasCaseProblem        bool
-	IsSpeakerHasProblem           bool
-	IsPowerButtonHasProblem       bool
-	IsBatterySocketHasProblem     bool
-	IsDeviceHasFreezingProblem    bool
-	IsBluetoothHasProblem         bool
-	IsWiFiHasProblem              bool
-	IsMicrophoneHasProblem        bool
-	IsDeviceHasCellularProblem    bool
+	IsScreenHasBrokenProblem      bool `json:"is_screen_has_broken_problem"`
+	IsScreenHasObscurationProblem bool `json:"is_screen_has_obscuration_problem"`
+	IsTouchScreenHasProblem       bool `json:"is_touch_screen_has_problem"`
+	IsScreenHasDeadPixelPixel     bool `json:"is_screen_has_dead_pixel_pixel"`
+	IsDeviceHasCaseProblem        bool `json:"is_device_has_case_problem"`
+	IsSpeakerHasProblem           bool `json:"is_speaker_has_problem"`
+	IsPowerButtonHasProblem       bool `json:"is_power_button_has_problem"`
+	IsBatterySocketHasProblem     bool `json:"is_battery_socket_has_problem"`
+	IsDeviceHasFreezingProblem    bool `json:"is_device_has_freezing_problem"`
+	IsBluetoothHasProblem         bool `json:"is_bluetooth_has_problem"`
+	IsWiFiHasProblem              bool `json:"is_wifi_has_problem"`
+	IsMicrophoneHasProblem        bool `json:"is_microphone_has_problem"`
+	IsDeviceHasCellularProblem    bool `json:"is_device_has_cellular_problem"`
 }
