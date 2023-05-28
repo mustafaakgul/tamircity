151085abe5c79c552c0d0af15a1bd079df0468a7
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-05-28 16:17:48 +0300

    Update logger.go

--- pkg/middleware/logger.go
+++ pkg/middleware/logger.go
@@ -7,7 +7,7 @@ import (
 
 func SentryLogger() {
 	err_sentry := sentry.Init(sentry.ClientOptions{
-		Dsn: "https://c23b897606e945f1aaa3f0440f523d34@o1418275.ingest.sentry.io/6766496",
+		Dsn: "YOUR_DSN",
 		// Set TracesSampleRate to 1.0 to capture 100%
 		// of transactions for performance monitoring.
 		// We recommend adjusting this value in production,
